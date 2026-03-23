c3a47b0823141a8607d080528118a2239b12aec5
emergentmind <emergentmind@users.noreply.github.com>
2026-03-22 21:39:28 -0600

    docs: add todos/fixmes

--- assets/pkg-deploy.sh
+++ assets/pkg-deploy.sh
@@ -11,15 +11,18 @@ verify_aws_session
 # load TOKENS/KEYS from .env
 load_env_vars
 
+# FIXME: this is where the package.yaml file is generated. needs to be a helper or
+# separate script
 echo "Creating package"
 aws cloudformation package \
   --template-file "$TEMPLATE_FILE" \
   --s3-bucket "$S3_BUCKET" \
-  --output-template-file "$PACKAGED_TEMPLATE_FILE"
+  --output-template-file "$PACKAGED_TEMPLATE_FILENAME"
 
 echo "Deploying package"
-aws cloudformation deploy \
-  --template-file "$PACKAGED_TEMPLATE_FILE" \
+aws cloudformation deploy
+# FIXME: packaged.yaml is generated when pkg-deploy.sh runs. See
+--template-file "$PACKAGED_TEMPLATE_FILE" \
   --stack-name "$STACK_NAME" \
   --capabilities CAPABILITY_IAM \
   --parameter-overrides GoogleBooksApiKey="$GOOGLE_BOOKS_API_KEY" GitHubToken="$GITHUB_TOKEN"
